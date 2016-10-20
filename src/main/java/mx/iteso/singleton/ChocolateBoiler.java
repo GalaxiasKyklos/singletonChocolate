05fe426a31423840c9282ff7e5eab15bcaa1081f
GalaxiasKyklos <saul_enrique-7285-96@hotmail.com>
2016-10-20 08:35:47 -0500

    first

---
+++ src/main/java/mx/iteso/singleton/ChocolateBoiler.java
@@ -0,0 +1,7 @@
+package mx.iteso.singleton;
+
+/**
+ * Created by Saúl on 20/10/2016.
+ */
+public class ChocolateBoiler {
+}
---
+++ src/test/java/mx/iteso/TestTest.java
@@ -0,0 +1,7 @@
+package mx.iteso;
+
+/**
+ * Created by Saúl on 20/10/2016.
+ */
+public class TestTest {
+}
