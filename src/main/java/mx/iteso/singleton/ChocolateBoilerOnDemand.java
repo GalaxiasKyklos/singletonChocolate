915d75cb34bb20f6858b6e044fe7283f72a4e6ed
Rudycc <rcarcue@gmail.com>
2016-10-21 17:45:07 -0500

    ondemand

---
+++ src/main/java/mx/iteso/singleton/ChocolateBoilerOnDemand.java
@@ -0,0 +1,49 @@
+package mx.iteso.singleton;
+
+/**
+ * Created by Rodolfo on 21/10/2016.
+ */
+public class ChocolateBoilerOnDemand {
+    private boolean empty;
+    private boolean boiled;
+
+    private static class Loader{
+         static ChocolateBoilerOnDemand instance=new ChocolateBoilerOnDemand();
+    }
+
+    private ChocolateBoilerOnDemand() {
+        empty = true;
+        boiled = false;
+    }
+
+
+    public ChocolateBoilerOnDemand getInstanceOnDemand() {
+        return Loader.instance;
+    }
+
+    public void fill() {
+        if (isEmpty()) {
+            empty = false;
+            boiled = false;
+            // fi ll the boiler with a milk/chocolate mixture
+        }
+    }
+    public void drain() {
+        if (!isEmpty() && isBoiled()) {
+            // drain the boiled milk and chocolate
+            empty = true;
+        }
+    }
+    public void boil() {
+        if (!isEmpty() && !isBoiled()) {
+            // bring the contents to a boil
+            boiled = true;
+        }
+    }
+    public boolean isEmpty() {
+        return empty;
+    }
+    public boolean isBoiled() {
+        return boiled;
+    }
+}
